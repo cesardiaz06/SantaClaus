7231d54cf51488c2a7259d9b4ee7e9c41c05fbdf
OscarPonce1 <oscar.ponce5918@alumnos.udg.mx>
2024-04-29 16:29:13 -0600

    Archivo Elfo.java
    hilo de elfo.

---
+++ santaClaus/src/santaclaus/Elfo.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package santaclaus;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Oscar Ponce
+ * Equipo santa claus
+ */
+public class Elfo extends Thread {
+
+    private int num;
+
+    public Elfo(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(toString() + " solicita ayuda.");
+        try {
+            if (Main.modelListaSalaEspera.size() == Main.CANT_ELFOS_ATENCION) {
+                ingresarSalaEspera();
+                Main.semSanta.release(1);
+            }else{
+                Main.semElfos.acquire();
+                ingresarSalaEspera();
+            }
+            
+        } catch (Exception e) {
+        }
+    }
+
+    
+    public void ingresarSalaEspera(){
+        System.out.println("El "+toString()+" ingreso a la sala de espera");
+    }
+    
+    public void solicitarAyuda() {
+        try {
+            System.out.println("Santa ayuda al " + toString());
+            Thread.sleep(1500);
+            System.out.println("Santa termino de ayudar al " + toString());
+            Main.modelListaElfos.removeElement(this);
+            Main.updateLabelElfos();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Elfo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Elfo " + (num + 1);
+    }
+
+}
